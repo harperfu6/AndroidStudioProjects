76998bdcfb2e6655591a2e0d8596d89176a5bb3a
harperfu6 <beharp8a8@gmail.com>
2022-09-17 11:38:14 +0900

    add wordsapp

---
+++ wordsapp/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.wordsapp
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+
+class LetterAdapter: RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+    private val list = ('A').rangeTo('Z').toList()
+
+    class LetterViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return LetterViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
---
+++ wordsapp/app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.wordsapp
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+
+class WordAdapter(
+    private val letterId: String,
+    context: Context
+): RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private val filteredWords: List<String>
+
+    init {
+        val words = context.resources.getStringArray(R.array.words).toList()
+
+        filteredWords = words
+            .filter { it.startsWith(letterId, ignoreCase = true) }
+            .shuffled()
+            .take(5)
+            .sorted()
+    }
+
+    class WordViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return WordViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val item = filteredWords[position]
+        holder.button.text = item
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return filteredWords.size
+    }
+}
