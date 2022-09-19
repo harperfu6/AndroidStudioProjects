b0b66094afc2979b8b91ba7bc67d9ed4d1a60885
harperfu6 <beharp8a8@gmail.com>
2022-09-19 10:46:32 +0900

    add menu handler

--- wordsapp/app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ wordsapp/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.wordsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
@@ -9,6 +13,7 @@ import com.example.wordsapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        chooseLayout()
+    }
+
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
         recyclerView.adapter = LetterAdapter()
     }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) {
+            return
+        }
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else
+                ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+    }
+
+    /**
+     * Initializes the [Menu] to be used with the current [Activity]
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+        return true
+    }
+
+    /**
+     * Determines how to handle interactions with the selected [MenuItem]
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
